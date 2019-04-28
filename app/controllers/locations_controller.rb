d3270baff1d4af2b7e8ab79b588dc3f1bbb05c1d
catsinspacesuits <simunro@hotmail.co.uk>
2019-04-28 12:28:27 +0200

    add devise views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,25 @@
 class LocationsController < ApplicationController
+  before_action :authenticate_user!
 
-def index
-  if params[:q].blank?
-    @coordinates = 'Please enter a query'
-    return
-  end
-
-  search = LocationiqApi.new('pk.d905eb6bf7b095ff4b883766bb357766').find_place(params[:q])
-
-  # Hash#dig will return nil if ANY part of the lookup fails
-  latitude = search.dig('searchresults', 'place', 'lat')
-  longitude = search.dig('searchresults', 'place', 'lon')
-
-  if latitude.nil? || longitude.nil?
-    # Output an error message if lat or lon is nil
-    @coordinates = 'No search results'
-  else
-    @coordinates = "#{latitude}, #{longitude}"
-  end
-end
+  def index
+    if params[:q].blank?
+      @message = 'Please enter an address in the field!'
+      return
+    end
   
+    search = LocationiqApi.new('pk.d905eb6bf7b095ff4b883766bb357766').find_place(params[:q])
+
+    # Hash#dig will return nil if ANY part of the lookup fails
+    latitude = search.dig('searchresults', 'place', 'lat')
+    longitude = search.dig('searchresults', 'place', 'lon')
+
+    if latitude.nil? || longitude.nil?
+      # Output an error message if lat or lon is nil
+      @coordinates = "We couldn't find a place by this name. Please enter a valid place name."
+    else
+      @coordinates = "Coordinates: " + "#{latitude}, #{longitude}"
+    end
+  end
 end
 
 
--- app/services/locationiq_api.rb
+++ app/services/locationiq_api.rb
@@ -8,12 +8,12 @@ class LocationiqApi
   end
 
   def find_place(street)
-    self.class.get(BASE_URI, query: @options.merge({ q: street })).parsed_response
+    self.class.get(BASE_URI, query: @options.merge({ q: street }))
   end 
 
   def handle_error
     if find_place.code.to_i = 200
-      find_place
+      find_place.parsed_response
     else
       raise "Couldn't connect to LocationIQ Api"
     end
