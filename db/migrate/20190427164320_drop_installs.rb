92d17fb78e8bef81ea3b30e35360f381a7f3eeba
catsinspacesuits <simunro@hotmail.co.uk>
2019-04-28 15:06:30 +0200

    delete locations migration files

--- db/migrate/20190427164320_drop_installs.rb
+++
@@ -1,4 +0,0 @@
-class DropInstalls < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_164350) do
+ActiveRecord::Schema.define(version: 2019_04_25_115915) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
